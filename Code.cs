5a9db488be9019380eff3b10e10208a2b9f5f987
Threemasks12 <jongeorgmagnussen@gmail.com>
2024-01-16 11:20:22 +0100

    Comittet

---
+++ Code.cs
@@ -0,0 +1,9 @@
+string testPassword = "123Today"
+
+Console.WriteLine("write a password");
+string awser = Console.readline();
+
+if (awser == testPassword)
+{
+    Console.WriteLine("Hello there");
+} 
